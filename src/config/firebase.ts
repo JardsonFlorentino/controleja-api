b10b1396fea52ba85646c617ca7498f7869b2b94
JardsonFlorentino <jardsonflorentino@gmail.com>
2025-12-01 21:35:59 -0300

    FIREBASE_PRIVATE_KEY_BASE64

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,27 +1,21 @@
 import admin from "firebase-admin";
 import { env } from "./env";
+import { Buffer } from "buffer";
 
-const initializeFirebaseAdmin = (): void => {
+const initializeFirebaseAdmin = () => {
   if (admin.apps.length > 0) return;
 
-  const { FIREBASE_PROJECT_ID, FIREBASE_PRIVATE_KEY, FIREBASE_CLIENT_EMAIL } = env;
+  const privateKey = Buffer
+    .from(env.FIREBASE_PRIVATE_KEY_BASE64, "base64")
+    .toString("utf8");
 
-  if (!FIREBASE_PROJECT_ID || !FIREBASE_PRIVATE_KEY || !FIREBASE_CLIENT_EMAIL) {
-    throw new Error("Firebase admin SDK is not properly configured.");
-  }
-
-  try {
-    admin.initializeApp({
-      credential: admin.credential.cert({
-        projectId: FIREBASE_PROJECT_ID,
-        clientEmail: FIREBASE_CLIENT_EMAIL,
-        privateKey: FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"), 
-      }),
-    });
-  } catch (error) {
-    console.error("Failed to initialize Firebase admin SDK.", error);
-    process.exit(1);
-  }
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: env.FIREBASE_PROJECT_ID,
+      clientEmail: env.FIREBASE_CLIENT_EMAIL,
+      privateKey,
+    }),
+  });
 };
 
 export default initializeFirebaseAdmin;
