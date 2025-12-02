5faa832498da7e04bd0af4f7e0ce1fed767e82a8
JardsonFlorentino <jardsonflorentino@gmail.com>
2025-12-02 11:35:10 -0300

    Correção variável de ambiente

--- src/app.ts
+++ src/app.ts
@@ -4,15 +4,17 @@ import routes from "./routes";
 import { env } from "./config/env";
 
 const app = Fastify({
- logger: {
-  // Log level baseado no ambiente
-  level: env.NODE_ENV === "dev" ? "info" : "error",
+logger: {
+
+ level: env.NODE_ENV === "dev" ? "info" : "error",
  },
 });
 
 const allowedOrigins = env.CORS_ORIGIN === "*"
- ? true // Permite todas as origens (somente para testes ou APIs públicas)
+ ? true 
  : env.CORS_ORIGIN.split(",").map((o) => o.trim());
+
+console.log('CORS Configured Allowed Origins:', allowedOrigins);
 
 app.register(cors, {
 
@@ -25,7 +27,6 @@ app.register(cors, {
 
  maxAge: 86400,
 });
-
 
 
 app.register(routes, { prefix: "/api" });
